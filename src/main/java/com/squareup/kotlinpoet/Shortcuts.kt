8ab1cc0a0ccb43f85c8bbb0914d727b034ab5cdc
Thijs de Haan <thijsdehaan71@hotmail.com>
2018-10-31 14:18:32 +0100

    adds DSL-marker
    adds DSL-marker using parameters

--- src/main/java/com/squareup/kotlinpoet/Shortcuts.kt
+++ src/main/java/com/squareup/kotlinpoet/Shortcuts.kt
@@ -6,6 +6,17 @@ import javax.lang.model.type.DeclaredType
 import javax.lang.model.util.Types
 import kotlin.reflect.KClass
 
+@Target(AnnotationTarget.TYPE)
+@DslMarker
+annotation class KotlinPoetDSL
+
+
+private typealias TypeSpecBlock = (@KotlinPoetDSL TypeSpec.Builder).() -> Unit
+private typealias PropertySpecBlock = (@KotlinPoetDSL PropertySpec.Builder).() -> Unit
+private typealias FunSpecBlock = (@KotlinPoetDSL FunSpec.Builder).() -> Unit
+private typealias FileSpecBlock = (@KotlinPoetDSL FileSpec.Builder).() -> Unit
+
+
 /*
  * Shortcuts for FileSpec
  */
@@ -13,7 +24,7 @@ import kotlin.reflect.KClass
 fun fileSpec(
   packageName: String,
   fileName: String,
-  block: FileSpec.Builder.() -> Unit
+  block: FileSpecBlock
 ) = FileSpec.builder(packageName, fileName).apply(block).build()
 
 /*
@@ -22,73 +33,73 @@ fun fileSpec(
 
 fun FileSpec.Builder.classSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.classBuilder(name).apply(block).build())
 
 fun FileSpec.Builder.classSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.classBuilder(className).apply(block).build())
 
 fun FileSpec.Builder.expectClassSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.expectClassBuilder(name).apply(block).build())
 
 fun FileSpec.Builder.expectClassSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.expectClassBuilder(className).apply(block).build())
 
 fun FileSpec.Builder.objectSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.objectBuilder(name).apply(block).build())
 
 fun FileSpec.Builder.objectSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.objectBuilder(className).apply(block).build())
 
 fun FileSpec.Builder.interfaceSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.interfaceBuilder(name).apply(block).build())
 
 fun FileSpec.Builder.interfaceSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.interfaceBuilder(className).apply(block).build())
 
 fun FileSpec.Builder.enumSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.enumBuilder(name).apply(block).build())
 
 fun FileSpec.Builder.enumSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.enumBuilder(className).apply(block).build())
 
 fun FileSpec.Builder.annotationSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.annotationBuilder(name).apply(block).build())
 
 fun FileSpec.Builder.annotationSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): FileSpec.Builder =
   addType(TypeSpec.annotationBuilder(className).apply(block).build())
 
@@ -99,73 +110,73 @@ fun FileSpec.Builder.annotationSpec(
 
 fun TypeSpec.Builder.classSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.classBuilder(name).apply(block).build())
 
 fun TypeSpec.Builder.classSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.classBuilder(className).apply(block).build())
 
 fun TypeSpec.Builder.expectClassSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.expectClassBuilder(name).apply(block).build())
 
 fun TypeSpec.Builder.cxpectClassSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.expectClassBuilder(className).apply(block).build())
 
 fun TypeSpec.Builder.objectSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.objectBuilder(name).apply(block).build())
 
 fun TypeSpec.Builder.objectSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.objectBuilder(className).apply(block).build())
 
 fun TypeSpec.Builder.interfaceSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.interfaceBuilder(name).apply(block).build())
 
 fun TypeSpec.Builder.interfaceSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.interfaceBuilder(className).apply(block).build())
 
 fun TypeSpec.Builder.enumSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.enumBuilder(name).apply(block).build())
 
 fun TypeSpec.Builder.enumSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.enumBuilder(className).apply(block).build())
 
 fun TypeSpec.Builder.annotationSpec(
   name: String,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.annotationBuilder(name).apply(block).build())
 
 fun TypeSpec.Builder.annotationSpec(
   className: ClassName,
-  block: TypeSpec.Builder.() -> Unit
+  block: TypeSpecBlock
 ): TypeSpec.Builder =
   addType(TypeSpec.annotationBuilder(className).apply(block).build())
 
@@ -177,7 +188,7 @@ fun FileSpec.Builder.propertySpec(
   name: String,
   type: TypeName,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): FileSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -188,7 +199,7 @@ fun FileSpec.Builder.propertySpec(
   name: String,
   type: Type,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): FileSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -199,7 +210,7 @@ fun FileSpec.Builder.propertySpec(
   name: String,
   type: KClass<*>,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): FileSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -214,7 +225,7 @@ fun TypeSpec.Builder.propertySpec(
   name: String,
   type: TypeName,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): TypeSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -225,7 +236,7 @@ fun TypeSpec.Builder.propertySpec(
   name: String,
   type: Type,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): TypeSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -236,7 +247,7 @@ fun TypeSpec.Builder.propertySpec(
   name: String,
   type: KClass<*>,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): TypeSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -247,7 +258,7 @@ fun TypeSpec.Builder.varPropertySpec(
   name: String,
   type: TypeName,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): TypeSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -259,7 +270,7 @@ fun TypeSpec.Builder.varPropertySpec(
   name: String,
   type: Type,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): TypeSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -271,7 +282,7 @@ fun TypeSpec.Builder.varPropertySpec(
   name: String,
   type: KClass<*>,
   vararg modifiers: KModifier,
-  block: (PropertySpec.Builder.() -> Unit) = {}
+  block: PropertySpecBlock = {}
 ): TypeSpec.Builder =
   addProperty(
     PropertySpec.builder(name, type, *modifiers)
@@ -285,7 +296,7 @@ fun TypeSpec.Builder.varPropertySpec(
 
 fun FileSpec.Builder.funSpec(
   name: String,
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): FileSpec.Builder =
   addFunction(FunSpec.builder(name).apply(block).build())
 
@@ -295,23 +306,23 @@ fun FileSpec.Builder.funSpec(
 
 fun TypeSpec.Builder.funSpec(
   name: String,
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): TypeSpec.Builder =
   addFunction(FunSpec.builder(name).apply(block).build())
 
 fun TypeSpec.Builder.primaryConstructorSpec(
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): TypeSpec.Builder =
   primaryConstructor(FunSpec.constructorBuilder().apply(block).build())
 
 fun TypeSpec.Builder.nonPrimaryConstructorSpec(
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): TypeSpec.Builder =
   addFunction(FunSpec.constructorBuilder().apply(block).build())
 
 fun TypeSpec.Builder.overridingSpec(
   method: ExecutableElement,
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): TypeSpec.Builder =
   addFunction(FunSpec.overriding(method).apply(block).build())
 
@@ -319,7 +330,7 @@ fun TypeSpec.Builder.overridingSpec(
   method: ExecutableElement,
   enclosing: DeclaredType,
   types: Types,
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): TypeSpec.Builder =
   addFunction(
     FunSpec.overriding(
@@ -332,12 +343,11 @@ fun TypeSpec.Builder.overridingSpec(
  */
 
 fun PropertySpec.Builder.setterSpec(
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): PropertySpec.Builder =
   setter(FunSpec.setterBuilder().apply(block).build())
 
 fun PropertySpec.Builder.getterSpec(
-  block: FunSpec.Builder.() -> Unit
+  block: FunSpecBlock
 ): PropertySpec.Builder =
   getter(FunSpec.getterBuilder().apply(block).build())
-
